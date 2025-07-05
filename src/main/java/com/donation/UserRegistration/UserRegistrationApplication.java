7ef5f35570538b37a470f708489d947890cd8ede
TanushreeShaw <shawtanushree17@gmail.com>
2025-07-05 17:28:08 +0530

    project structure files

---
+++ src/main/java/com/donation/UserRegistration/UserRegistrationApplication.java
@@ -0,0 +1,15 @@
+package com.donation.UserRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationApplication {
+
+	public static void main(String[] args) {
+		//dummy commit
+		SpringApplication.run(UserRegistrationApplication.class, args);
+
+	}
+
+}
